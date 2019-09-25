8ed7741b11365a1eef72f60a997b85b4b644ca75
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2019-09-25 15:06:38 -0400

    Add select

---
+++ src/Scopes/Select.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dbt\Query\Scopes;
+
+use Dbt\Query\AbstractScope;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Query\Expression;
+
+class Select extends AbstractScope
+{
+    /** @var string[] */
+    private $columns = [];
+
+    public function __construct (string ...$columns)
+    {
+        foreach ($columns as $column) {
+            $this->columns[] = new Expression($column);
+        }
+    }
+
+    public function apply (Builder $query): Builder
+    {
+        return $query->select($this->columns);
+    }
+}
